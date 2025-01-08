3d0b9f5c8136b878439b5ef20ee4db5dee94eef6
Suhyun751207 <23s607h0659@gmail.com>
2025-01-09 02:06:58 +0900

    Add files via upload

---
+++ js/system.js
@@ -0,0 +1,20 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCXtP_K6pkRo-ioLUsI76DozgDsQxHgY0k",
+//     authDomain: "smc-project-22e7e.firebaseapp.com",
+//     databaseURL: "https://smc-project-22e7e-default-rtdb.firebaseio.com",
+//     projectId: "smc-project-22e7e",
+//     storageBucket: "smc-project-22e7e.appspot.com",
+//     messagingSenderId: "137462129835",
+//     appId: "1:137462129835:web:297698f42045962dcf43ee",
+//     measurementId: "G-6V2SYEQ8DR"
+// };
+const firebaseConfig = {
+    apiKey: "AIzaSyA_Co_CZAlcv33j3ByLSoHif_e8NBFypGo",
+    authDomain: "html1-project.firebaseapp.com",
+    databaseURL: "https://html1-project-default-rtdb.firebaseio.com",
+    projectId: "html1-project",
+    storageBucket: "html1-project.appspot.com",
+    messagingSenderId: "905909966724",
+    appId: "1:905909966724:web:eb02b9499f80f90557efd1",
+    measurementId: "G-NX0Z7VEZS1"
+};
